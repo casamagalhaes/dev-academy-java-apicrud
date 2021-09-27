be9e5eb0f99ae3111aff223a3f07d0f890bac9ea
danieleandrade <danyelebatista@gmail.com>
2021-09-27 18:21:57 -0300

    adição de novas dependencias e atualização dos arquivos

--- src/main/java/br/com/cm/workshop/apicrud/repository/BebidaRepository.java
+++ src/main/java/br/com/cm/workshop/apicrud/repository/BebidaRepository.java
@@ -6,7 +6,4 @@ import org.springframework.stereotype.Repository;
 import br.com.cm.workshop.apicrud.model.Bebida;
 
 @Repository
-public interface BebidaRepository extends JpaRepository<Bebida, Long> {
-
-
-}
+public interface BebidaRepository extends JpaRepository<Bebida, Long> {}
