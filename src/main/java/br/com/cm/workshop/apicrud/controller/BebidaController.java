e3d1d78a0351f137ca4e7b5759ef0892abfdf81f
Sidney Nobre <sidnobre.sdn@gmail.com>
2021-09-26 23:43:09 -0300

    projeto base

---
+++ src/main/java/br/com/cm/workshop/apicrud/controller/BebidaController.java
@@ -0,0 +1,8 @@
+package br.com.cm.workshop.apicrud.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BebidaController {
+    
+}
---
+++ src/main/java/br/com/cm/workshop/apicrud/service/BebidaService.java
@@ -0,0 +1,9 @@
+package br.com.cm.workshop.apicrud.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BebidaService {
+    
+}
+
